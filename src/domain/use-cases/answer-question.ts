d780a924df3da89817fcd38cc571c10aa6f3f6c4
Savio-Anjos <savioanjos2020@gmail.com>
2024-02-14 00:49:01 -0300

    feat: :sparkles: Added Path aliases and Vitest globals

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,4 @@
-import { UniqueEntityID } from "../../core/entities/unique-entity-id";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
 
