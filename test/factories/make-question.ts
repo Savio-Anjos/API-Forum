58b8446e0da0b91b2fd94d372f29ac965805a374
Savio-Anjos <savioanjos2020@gmail.com>
2024-02-15 15:13:44 -0300

    feat: :sparkles: Generating dummy data

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -2,15 +2,22 @@ import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 import { QuestionProps } from "./../../src/domain/forum/enterprise/entities/question";
+import faker from "@faker-js/faker";
 
-export function makQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: "Example Question",
-    slug: Slug.create("example-question"),
-    content: "Example content",
-    ...override,
-  });
+export function makQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      slug: Slug.create("example-question"),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id
+  );
 
   return question;
 }
