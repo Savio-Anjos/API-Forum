3ebf529fbb56f6ec346c32ed5b7594dfcd9a3b52
Savio-Anjos <savioanjos2020@gmail.com>
2024-03-28 14:30:29 -0300

    feat: :sparkles: Added  Functional Error Handling

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,3 +1,4 @@
+import { Either, left, right } from "@/core/either";
 import { AnswerCommentsRepository } from "../repositories/answer-comments-repository";
 
 interface DeleteAnswerCommentRequest {
@@ -5,7 +6,7 @@ interface DeleteAnswerCommentRequest {
   answerCommentId: string;
 }
 
-interface DeleteAnswerCommentUseCaseResponse {}
+type DeleteAnswerCommentUseCaseResponse = Either<string, {}>;
 
 export class DeleteAnswerCommentUseCase {
   constructor(private answersCommentRepository: AnswerCommentsRepository) {}
@@ -18,15 +19,15 @@ export class DeleteAnswerCommentUseCase {
     );
 
     if (!answerComment) {
-      throw new Error("Answer comment not found.");
+      return left("Answer comment not found.");
     }
 
     if (answerComment.authorId.toString() !== authorId) {
-      throw new Error("Not allowed");
+      return left("Not allowed");
     }
 
     await this.answersCommentRepository.delete(answerComment);
 
-    return {};
+    return right({});
   }
 }
